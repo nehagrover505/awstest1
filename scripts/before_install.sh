ef46863facd4ee4e777c9a822000e2b1443586ff
Neha Grover - iGuru <131132217+nehagrover505@users.noreply.github.com>
2023-09-30 17:17:48 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 # This script is executed before the new version of your application is installed.
 
 # Stop the application if it's running
-sudo systemctl stop httpd
+sudo systemctl stop nginx
 
 # Perform any pre-installation tasks, e.g., installing dependencies
 # sudo yum install -y some-dependency
