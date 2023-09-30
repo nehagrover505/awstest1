12f45876150d011e791457f28d41440c5b763a6a
Neha Grover - iGuru <131132217+nehagrover505@users.noreply.github.com>
2023-09-30 17:15:18 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Example before_install.sh script
+# This script is executed before the new version of your application is installed.
+
+# Stop the application if it's running
+sudo systemctl stop httpd
+
+# Perform any pre-installation tasks, e.g., installing dependencies
+# sudo yum install -y some-dependency
+
