a1b44dc1c3b1bdb45c115e386209b20431c6d374
Neha Grover - iGuru <131132217+nehagrover505@users.noreply.github.com>
2023-09-30 17:16:04 +0530

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Example after_install.sh script
+# This script is executed after the new version of your application is installed.
+
+# Start or restart the application service
+sudo systemctl start httpd
+
+# Perform any post-installation configuration, e.g., updating application settings
